84341c06770cc72ce32e9653a34c0fc2ed1837c7
JoaquinCendoya <104681657+JoaquinCendoya@users.noreply.github.com>
2025-04-14 22:42:07 -0300

    Program.java cambio inicializacion de vista principal

--- src/views/Program.java
+++ src/views/Program.java
@@ -10,7 +10,7 @@ public class Program {
 
     public static void main(String[] args) throws IllegalArgumentException, InvalidPropertiesFormatException {
         Persistencia.inicializar();
-        ListarAnimalesView view = new ListarAnimalesView();
+        Principal view = new Principal();
         view.setVisible(true);
     }
 }
